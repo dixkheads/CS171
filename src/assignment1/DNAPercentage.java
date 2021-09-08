df7b3a88cc352ea88695ddf23247da161ca5f75f
dixkhead <gyrocopterfaith@hotmail.com>
2021-09-07 21:06:32 -0400

    test

---
+++ src/assignment1/DNAPercentage.java
@@ -0,0 +1,57 @@
+package assignment1;
+
+import java.util.Random;
+
+public class DNAPercentage 
+{
+	static String genMers(double Apos, double Cpos, double Gpos, double Tpos)
+	{
+		Random rd = new Random();
+		String oup = new String();
+		for(int i = 0; i < 3; i++)
+		{
+			double poss = rd.nextDouble();
+			if(poss < Apos)
+			{
+				oup += 'A';
+			}
+			else if (poss < Cpos + Apos)
+			{
+				oup += 'C';
+			}
+			else if (poss < Gpos + Cpos + Apos)
+			{
+				oup += 'G';
+			}
+			else
+			{
+				oup += 'T';
+			}
+		}
+		return oup;
+	}
+	
+	public static void main(String[] args)
+	{
+		int count1 = 0;
+		for(int i = 0; i < 1000; i++)
+		{
+			if(genMers(0.25, 0.25, 0.25, 0.25).equals("AAA"))
+			{
+				count1++;
+			}
+		}
+		//Expected 0.25^3*1000
+		System.out.println("The possibility for PartI: " + count1);
+		int count2 = 0;
+		for(int i = 0; i < 1000; i++)
+		{
+			if(genMers(0.12, 0.38, 0.39, 0.11).equals("AAA"))
+			{
+				count2++;
+			}
+		}
+		//Expected 0.12^3*1000
+		System.out.println("The possibility for PartII: " + count2);
+	}
+}
--- src/randomJunk/PrimeNum.java
+++ src/randomJunk/PrimeNum.java
@@ -7,3 +7,25 @@ public class PrimeNum
 		
 	}
 }
+
+
+
+abstract class A {
+  abstract int increment(int x);
+}
+
+class B extends A {
+  int increment(int x) { return ++x; }
+}
+
+abstract class BC extends A {
+	  int increment(int x) { return ++x; }
+	}
+
+class BAC extends A {
+	  int decrement(int y) { return --y; }
+	}
+
+abstract class BADAD extends A {
+	  // empty body ...
+	}
