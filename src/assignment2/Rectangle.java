32b774e579ccf503be1cd42d6670c85f61625d07
dixkheads <gyrocopterfaith@hotmail.com>
2021-09-15 21:54:17 -0400

    temp

--- src/assignment2/Rectangle.java
+++ src/assignment2/Rectangle.java
@@ -11,38 +11,94 @@ public class Rectangle {
 		// TODO: [3 pts] Write a basic constructor that creates a rectangle with
 		// side lengths 1 whose bottom left corner is at (0.0,0.0)
 		// ... Add your code here ... //
-		
+		public Rectangle()
+		{
+			this.x = 0.0;
+			this.y = 0.0;
+			this.L = 1.0;
+			this.H = 1.0;
+		}
 
 		// TODO: [3 pts] Now write a constructor that takes all four inputs,
 		// named Ell, Eich, Ex, and Why for L, H, x, and y, respectively.
 		// ... Add your code here ... //
+		public Rectangle(double Ell, double Eich, double Ex, double Why)
+		{
+			this.x = Ex;
+			this.y = Why;
+			this.L = Ell;
+			this.H = Eich;
+		}
 
 		// TODO: [2 pts] Write a method called getLength which returns the length.
 		// ... Add your code here ... //
+		public double getLength()
+		{
+			return this.L;
+		}
 
 		// TODO: [2 pts] Write a method called getHeight which returns the height.
 		// ... Add your code here ... //
+		public double getHeight()
+		{
+			return this.H;
+		}
 
 		// TODO: [2 pts] Write a method called setLength which takes as input a double
 		// called Ell and uses it to set the length of the rectangle.
 		// ... Add your code here ... //
+		public void setLength(double Length)
+		{
+			this.L = Length;
+		}
 
 		// TODO: [2 pts] Write a method called setHeight which takes as input a double
 		// called Eich and uses it to set the height of the rectangle.
 		// ... Add your code here ... //
+		public void setHeight(double Height)
+		{
+			this.H = Height;
+		}
 
 		// TODO: [2 pts] Write a method called perimeter which computes and returns
 		// the perimeter of the rectangle.
 		// ... Add your code here ... //
+		public double perimeter()
+		{
+			return L + H + L + H;
+		}
 
 		// TODO: [2 pts] Write a method called area which computes and returns the
 		// area of the rectangle.
 		// ... Add your code here ... //
+		public double area() 
+		{
+			return L * H;
+		}
 
 		// TODO: [2 pts] Override the method equals which is inherited from class Object
 		// (similar to what we did in class Employee), and make it return true if the
 		// two rectangles have equal areas, false otherwise
 		// Important: Use the @Override annotation
 		// ... Add your code here ... //
+		@Override
+		public boolean equals(Object another)
+		{
+			if(another instanceof Rectangle)
+			{
+				if(((Rectangle) another).area() == this.area())
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
 
 }
--- src/assignment2/ShapeTester.java
+++ src/assignment2/ShapeTester.java
@@ -1,3 +1,5 @@
+package assignment2;
+
 public class ShapeTester {
 
   // !!!Important!!!
@@ -8,31 +10,97 @@ public class ShapeTester {
   // the circle is bigger than (or equal to) the area of the rectangle,
   // false otherwise.
   // ... Add your code here ... //
+	public static boolean isLarger(Circle circle, Rectangle rectangle)
+	{
+		if(circle.getArea() >= rectangle.area())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
   // TODO: [2 pts] Write the method longerPerim which takes as input
   // a Circle object followed by a Rectangle object and returns
   // the length of the perimeter of the longer of the two objects.
   // ... Add your code here ... //
+	public static double longerPerim(Circle circle, Rectangle rectangle)
+	{
+		if(circle.getCircumference() > rectangle.perimeter())
+		{
+			return circle.getCircumference();
+		}
+		else
+		{
+			return rectangle.perimeter();
+		}
+	}
 
   // TODO: [2 pts] Write another version of the method longerPerim which has the
   // same name and functionality but it takes as input a Rectangle object
   // followed by a Circle object. The method also returns the length of
   // the perimeter of the longer of the two objects.
   // ... Add your code here ... //
+	public static double longerPerim(Rectangle rectangle, Circle circle)
+	{
+		if(circle.getCircumference() > rectangle.perimeter())
+		{
+			return circle.getCircumference();
+		}
+		else
+		{
+			return rectangle.perimeter();
+		}
+	}
 
   // TODO: [2 pts] Write the method largerArea which takes as input
   // a Circle object followed by a Rectangle object and returns
   // the area of the larger of the two objects.
   // ... Add your code here ... //
+	public static double largerArea(Rectangle rectangle, Circle circle)
+	{
+		if(circle.getArea() > rectangle.area())
+		{
+			return circle.getArea();
+		}
+		else
+		{
+			return rectangle.area();
+		}
+	}
 
   // TODO: [2 pts] Writer another version of the method largerArea which has the
   // same name and functionality but it takes as input a Rectangle object
   // followed by a Circle object.
   // ... Add your code here ... //
+	public static double largerArea(Circle circle, Rectangle rectangle)
+	{
+		if(circle.getArea() > rectangle.area())
+		{
+			return circle.getArea();
+		}
+		else
+		{
+			return rectangle.area();
+		}
+	}
 
   // TODO: [10 pts] Write the method containsCenter which takes as input two circles,
   // and returns true if the first circle contains the center of the second circle,
   // false otherwise.
   // ... Add your code here ... //
+	public static boolean containsCenter(Circle circle1, Circle circle2)
+	{
+		if((circle1.x - circle2.x) * (circle1.x - circle2.x) + (circle1.y - circle2.y) * (circle1.y - circle2.y) < circle1.radius * circle1.radius)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 }
